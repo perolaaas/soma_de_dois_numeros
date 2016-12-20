1b385b1f3f8a27eb8e85c01c5117b8bca2a2f598
PEROLA <PEROLA@PEROLA>
2016-12-20 08:02:51 -0200

    Adicionar arquivos de projeto.

---
+++ soma_de_dois_numeros/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soma_de_dois_numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variáveis
+            int n1 = 0;
+            int n2 = 0;
+            int soma = 0;
+            Console.Write("Digite o primeiro número: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            soma = n1 + n2;
+
+            Console.Write("A soma dos números é: ");
+            Console.Write(soma);
+
+            Console.ReadKey();
+
+        }
+    }
+}
